b79a92987939ffb738d89dde659ed7ba87cef6e6
Wildflocadventures <web@wildfloc.com>
2025-02-10 13:32:52 +0530

    s

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -52,7 +52,7 @@ export const Navbar = ({ session, userProfile }: NavbarProps) => {
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0 cursor-pointer" onClick={() => navigate('/')}>
             <h1 className="text-xl font-bold text-blue-600">Wildfloc Adventures</h1>
-             <img src="./Wildfloc.jpg"></img>
+             <img src="Wildfloc.jpg"></img>
           </div>
           
           <div className="flex gap-4">
