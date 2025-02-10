4ab54fada7c54fbacbab6cc828249e64ebe35f19
Wildflocadventures <web@wildfloc.com>
2025-02-10 13:46:41 +0530

    s

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { LogIn, UserPlus, LogOut, Car, List } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 import { supabase } from "@/integrations/supabase/client";
-import logo from './wildlfoc_ogo.jpg';
+import logo from './logo.jpg';
 
 
 interface NavbarProps {
